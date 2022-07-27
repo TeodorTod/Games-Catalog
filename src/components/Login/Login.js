aa19acb02a8041d568f5283b545b36049f008bc1
Artos Web <teodor.at.tod@gmail.com>
2022-07-28 00:25:50 +0300

    add login functionallity

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,24 @@
-import { Link } from "react-router-dom";
+import { login } from "../../services/authService";
+
 
 const Login = () => {
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const {
+            email,
+            password
+        } = Object.fromEntries(new FormData(e.target));
+
+        
+        login(email, password)
+            .then(authData => {
+                console.log(authData);
+            })
+    };
     return (
         <section id="login-page" className="auth">
-            <form id="login">
+            <form id="login" onSubmit={onSubmit}>
                 <div className="container">
                     <div className="brand-logo" />
                     <h1>Login</h1>
@@ -16,10 +31,10 @@ const Login = () => {
                     />
                     <label htmlFor="login-pass">Password:</label>
                     <input type="password" id="login-password" name="password" />
-                    <input type="submit" className="btn submit" defaultValue="Login" />
+                    <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
                         <span>
-                            If you don't have profile click <Link to="/register">here</Link>
+                            If you don't have profile click <a to="/register">here</a>
                         </span>
                     </p>
                 </div>
