53b6b78fc5c19dafa79bfecb3ab738536011ddfa
Artos Web <teodor.at.tod@gmail.com>
2022-08-08 23:17:26 +0300

    edit catalog

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -12,7 +12,7 @@ const Catalog = () => {
 
             {games.length > 0
                 ? games.map(x => <CatalogItem key={x._id} game={x} />)
-                : <h3 className="no-articles">No articles yet</h3>
+                : <h3 className="no-articles">No articles yet!</h3>
             }
         </section>
     );
